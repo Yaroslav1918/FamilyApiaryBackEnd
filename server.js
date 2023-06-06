33b3925c0448c7d69c68095a48844350920cb107
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2023-06-06 21:50:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 require("dotenv").config();
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3038, () => {
+    app.listen(PORT, () => {
       console.log("Server running. Use our API on port: 3038");
     });
   })
