f997cdbe0ebb4b1ef8c6cb679b4162246a860b4e
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2023-06-11 15:22:37 +0300

    added google auth

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const fs = require('fs');
 const authRouter = require("./routes/api/auth");
 const productsRouter = require("./routes/api/products");
 const app = express();
-
+const { OAuth2Client } = require("google-auth-library");
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
+const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+const { User } = require("./models/user");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { v4 } = require("uuid");
+const { createError } = require("./helpers");
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
@@ -24,6 +29,37 @@ app.get('/api/translations/:lng', (req, res) => {
   const translation = JSON.parse(fileContents);
   res.json(translation);
 
+});
+app.post("/api/google-login", async (req, res) => {
+  const { token } = req.body;
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  });
+  const { name, email, picture } = ticket.getPayload();
+const result = await User.findOne({ email });
+if (result) {
+  throw createError(409, "Email already exist");
+}
+const hashPassword = await bcrypt.hash(email, 10);
+const avatarURL = gravatar.url(email);
+const verificationToken = v4();
+await User.create({
+  name,
+  email,
+  password: hashPassword,
+  verificationToken,
+  avatarURL,
+});
+
+res.status(201).json({
+  user: {
+    name,
+    email,
+  },
+});
+  res.status(201);
+  res.json({ name, email, picture });
 });
 
 app.use((req, res) => {
