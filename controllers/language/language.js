5608c54087abaa38c590551ed89d53abb1de833d
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2023-06-22 21:46:01 +0300

    Update language.js

--- controllers/language/language.js
+++ controllers/language/language.js
@@ -1,18 +1,21 @@
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const language = async (req, res) => {
   const { lng } = req.params;
-  const filePath = path.join(
-    __dirname,
-    "public",
-    "locales",
-    lng,
-    "translation.json"
-  );
-  const fileContents = fs.readFileSync(filePath, "utf8");
-  const translation = JSON.parse(fileContents);
-  res.json(translation);
+
+  const filePath = path.join( "public", "locales", lng, "translation.json");
+   console.log("🚀 ~ file: language.js:15 ~ language ~ translation:", filePath);
+  try {
+    const fileContents = await fs.readFile(filePath, "utf8");
+    const translation = JSON.parse(fileContents);
+ 
+    res.json(translation);
+  } catch (error) {
+    // Handle error while reading the file
+    console.error("Error reading file:", error);
+    res.status(500).json({ error: "Failed to read file" });
+  }
 };
 
 module.exports = language;
